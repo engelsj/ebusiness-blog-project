f25e7f068dc074dc8f228a41225382e44e83e344
Unknown <lpc477@gmail.com>
2019-10-08 16:44:09 +0100

    Homepage and full architecture support
    Created home.html as the first page, and created the views folder to render the templates to the views

--- ebusiness_project_django/ebusiness_project_django/settings.py
+++ ebusiness_project_django/ebusiness_project_django/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ebusiness_project_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
